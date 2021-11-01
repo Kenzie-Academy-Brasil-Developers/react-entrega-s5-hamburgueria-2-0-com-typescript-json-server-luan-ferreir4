01136cc4d7fe085215ebf84299236823e9c88cb2
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-01 00:39:19 -0300

    feat: added elements

--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -3,6 +3,14 @@ import { Link } from "react-router-dom"
 export const SignInPage = () => {
     return(
         <div>
+            <div>
+            <h1>Burguer</h1>
+            <h3>Kenzie</h3>
+            </div>
+            <div>
+                <div>icon</div>
+                <p>A vida é como um sanduíche, é preciso recheá-la com os melhores ingredientes.</p>
+            </div>
             <LoginForm/>
             <div>
                 <small>Não possui uma conta?</small>
