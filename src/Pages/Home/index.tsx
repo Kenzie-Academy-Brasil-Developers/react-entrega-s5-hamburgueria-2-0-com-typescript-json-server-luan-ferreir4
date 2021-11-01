bb18a731871867efdc3b0b56cae4ea1787843fd1
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-11-01 00:34:42 -0300

    refactor: deleted unused imports

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
 import { Cart } from "../../Components/Cart";
 import { ProductsList } from "../../Components/ProductsList";
-import { UseProducts } from "../../Providers/Products";
 import { UseSignIn } from "../../Providers/SignIn";
-import { UseUser } from "../../Providers/User";
 
 export const HomePage = () => {
  
-
   const { signOut } = UseSignIn();
-
-  
-  
-
 
   return (
     <div>
